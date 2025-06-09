2b7898d8089a1c6c8e71ed786be12c06d293437f
bxwpxx <2285329960@qq.com>
2025-06-09 11:19:53 +0800

    Delete Server directory

--- Server/src/main/java/com/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.service;
-
-import com.domain.vo.LoginResponse;
-
-import java.sql.SQLException;
-
-public interface AuthService {
-    LoginResponse authenticate(String username, String password) throws SQLException;
-}
